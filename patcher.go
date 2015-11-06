90ea7462b5c3a4a4d6f08965fb54de3b88dd1366
lysu <sulifx@gmail.com>
2015-11-06 23:59:43 +0800

    refactor

--- patcher.go
+++ patcher.go
@@ -10,101 +10,172 @@ import (
 	"unicode/utf8"
 )
 
-// Patcher use to Patch values of a struct
-type Patcher interface {
-	PatchIt(it interface{}, patches map[string]interface{}) []string
+// Patcher use to patch in memory struct with path
+type Patcher struct {
+	Cache map[string]reflect.Value
 }
 
-// NewPatcher use to create new patcher
-func NewPatcher() Patcher {
-	return &reflectPatcher{
-		cache: make(map[string]reflect.Value),
-	}
-}
+type Path string
 
-var NumberType reflect.Type = reflect.TypeOf(json.Number(""))
-
-type reflectPatcher struct {
-	cache map[string]reflect.Value
-}
-
-func (p *reflectPatcher) tokenize(path string) []string {
+func (p Path) Tokenize() []string {
 	var toks []string
-	for _, tok := range strings.Split(path, ".") {
+	for _, tok := range strings.Split(string(p), ".") {
 		toks = append(toks, upperFirst(tok))
 	}
 	return toks
 }
 
-func (p *reflectPatcher) PatchIt(target interface{}, patches map[string]interface{}) []string {
-	var patchedSegs []string
-	targetValue := reflect.ValueOf(target)
-	for path, value := range patches {
-		tokens := p.tokenize(path)
-		patchedSegs = append(patchedSegs, tokens[0])
-		v, vname := p.patchRecursive("", targetValue, tokens, "", value)
-		if v == nil {
-			panic(fmt.Sprintf("Can found field %v to patch", tokens))
-		}
-		if !v.CanSet() {
-			panic("12121")
-		}
-		valueType := reflect.TypeOf(value)
-		if valueType == NumberType {
-			nv := value.(json.Number)
-			switch v.Kind() {
-			case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-				n, err := strconv.ParseInt(string(nv), 10, 64)
-				if err != nil || v.OverflowInt(n) {
-					panic(fmt.Sprintf("field: %s, number %v as %s patch failure err: %v", vname, value, v.Type(), err))
-				}
-				v.SetInt(n)
-				continue
-
-			case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
-				n, err := strconv.ParseUint(string(nv), 10, 64)
-				if err != nil || v.OverflowUint(n) {
-					panic(fmt.Sprintf("field: %v, number %v as %s patch failure err: %v", vname, value, v.Type(), err))
-				}
-				v.SetUint(n)
-				continue
-
-			case reflect.Float32, reflect.Float64:
-				n, err := strconv.ParseFloat(string(nv), v.Type().Bits())
-				if err != nil || v.OverflowFloat(n) {
-					panic(fmt.Sprintf("field: %v, number %v as %s patch failure err: %v", value, v.Type(), err))
-				}
-				v.SetFloat(n)
-				continue
-			default:
-				panic(fmt.Sprintf("field %s use value %v to patch %s type", vname, value, v.Kind()))
-			}
-		} else {
-			vv := reflect.ValueOf(value)
-			if v.Type() != vv.Type() {
-				panic(fmt.Sprintf("field %s use value %v to patch %s type", vname, vv.Type(), v.Type()))
-			}
-			v.Set(vv)
-		}
+func (p Path) Segment() string {
+	idx := strings.Index(string(p), ".")
+	if idx == -1 {
+		return ""
 	}
-	return patchedSegs
+	return upperFirst(string(p)[:idx])
 }
 
-func (p *reflectPatcher) patchRecursive(fieldName string, targetValue reflect.Value, tokens []string, path string, value interface{}) (*reflect.Value, string) {
+func upperFirst(s string) string {
+	if s == "" {
+		return ""
+	}
+	r, n := utf8.DecodeRuneInString(s)
+	return string(unicode.ToUpper(r)) + s[n:]
+}
+
+// NumberType avada patch api use this type to deserialize request
+var NumberType = reflect.TypeOf(json.Number(""))
+
+// DoPatch do patch job
+// and generate segment update statstic
+func (p *Patcher) DoPatch(target interface{}, paths map[Path]interface{}) ([]string, error) {
+
+	targetValue := reflect.ValueOf(target)
+
+	for path, value := range paths {
+
+		fieldName, fieldValue, err := p.locateField(targetValue, path)
+		if err != nil {
+			return nil, err
+		}
+
+		err = p.setValue(fieldName, fieldValue, value)
+		if err != nil {
+			return nil, err
+		}
+
+	}
+
+	return p.updatedSegments(p.Segments()), nil
+}
+
+func (p *Patcher) locateField(modified reflect.Value, path Path) (string, *reflect.Value, error) {
+	tokens := path.Tokenize()
+	fieldValue, fieldName := p.patchRecursive("", modified, tokens, "")
+	if fieldValue == nil {
+		return "", nil, fmt.Errorf("无法匹配的Patch表达式: %s", string(path))
+	}
+	if !fieldValue.CanSet() {
+		return fieldName, fieldValue, fmt.Errorf("Patch表达式%s对应的字段%s不可写", string(path), fieldName)
+	}
+	return fieldName, fieldValue, nil
+}
+
+func (p *Patcher) setValue(fieldName string, fieldValue *reflect.Value, rightValue interface{}) error {
+
+	rvType := reflect.TypeOf(rightValue)
+
+	if rvType == NumberType {
+
+		nv := rightValue.(json.Number)
+
+		switch fieldValue.Kind() {
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			n, err := strconv.ParseInt(string(nv), 10, 64)
+			if err != nil || fieldValue.OverflowInt(n) {
+				return fmt.Errorf("field %s number %v as %s patch failure err: %v", fieldName, rightValue, fieldValue.Type(), err)
+			}
+			fieldValue.SetInt(n)
+			return nil
+
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+			n, err := strconv.ParseUint(string(nv), 10, 64)
+			if err != nil || fieldValue.OverflowUint(n) {
+				return fmt.Errorf("field %s number %v as %s patch failure err: %v", fieldName, rightValue, fieldValue.Type(), err)
+			}
+			fieldValue.SetUint(n)
+			return nil
+
+		case reflect.Float32, reflect.Float64:
+			n, err := strconv.ParseFloat(string(nv), fieldValue.Type().Bits())
+			if err != nil || fieldValue.OverflowFloat(n) {
+				return fmt.Errorf("field %s number %v as %s patch failure err: %v", fieldName, fieldValue, fieldValue.Type(), err)
+			}
+			fieldValue.SetFloat(n)
+			return nil
+
+		default:
+			return fmt.Errorf("field %s use value %v to patch %s type", fieldName, fieldValue, fieldValue.Kind())
+		}
+	} else {
+		if rvType != fieldValue.Type() {
+			return fmt.Errorf("field %s use value %v to patch %s type", fieldName, rvType, fieldValue.Type())
+		}
+		fieldValue.Set(reflect.ValueOf(rightValue))
+	}
+	return nil
+}
+
+func (p *Patcher) updatedSegments(startPaths []string) []string {
+	updatedSegments := make(segmentSet)
+	for _, segment := range startPaths {
+		switch segment {
+		case "UserBasic":
+			updatedSegments.put("UserBasic")
+		case "Register":
+			updatedSegments.put("UserRegister", "RegAudit")
+		case "IdCard":
+			updatedSegments.put("UserIdCard", "IdCardAudit")
+		case "DriverLic":
+			updatedSegments.put("UserDriverLic", "DriverLicAudit")
+		case "UserQualification":
+			updatedSegments.put("UserQualification", "QualificationAudit")
+		case "RegAudit":
+			updatedSegments.put("RegAudit")
+		case "IdCardAudit":
+			updatedSegments.put("IdCardAudit")
+		case "DriverLicAudit":
+			updatedSegments.put("DriverLicAudit")
+		case "QualificationAudit":
+			updatedSegments.put("QualificationAudit")
+		case "Ownership":
+			updatedSegments.put("Ownership")
+		case "Vehicle":
+			updatedSegments.put("Ownership", "VehicleBasic", "VehicleLicense", "CompulsoryInsurance", "VehicleInsurance") // maybe some opt on this.
+		case "Image":
+			updatedSegments.put(
+				"Image",
+				"UserRegister", "RegAudit", "UserIdCard", "IdCardAudit", "UserDriverLic", "DriverLicAudit", "UserQualification", "QualificationAudit",
+				"Ownership", "VehicleBasic", "VehicleLicense", "CompulsoryInsurance", "VehicleInsurance",
+			)
+		}
+	}
+	return updatedSegments.asSlice()
+}
+
+func (p *Patcher) patchRecursive(fieldName string, targetValue reflect.Value, tokens []string, path string) (*reflect.Value, string) {
 	switch targetValue.Kind() {
 	case reflect.Ptr:
 		originalValue := targetValue.Elem()
-		return p.patchRecursive(fieldName, originalValue, tokens, path, value)
+		return p.patchRecursive(fieldName, originalValue, tokens, path)
 	case reflect.Interface:
 		originalValue := targetValue.Elem()
-		return p.patchRecursive(fieldName, originalValue, tokens, path, value)
+		return p.patchRecursive(fieldName, originalValue, tokens, path)
 	case reflect.Struct:
 		typeOfT := targetValue.Type()
-		for i := 0; i < targetValue.NumField(); i += 1 {
+		for i := 0; i < targetValue.NumField(); i++ {
 			currentTok := tokens[0]
 			if typeOfT.Field(i).Name == currentTok {
 				path = path + "." + currentTok
-				return p.patchRecursive(typeOfT.Field(i).Name, targetValue.Field(i), tokens[1:], path, value)
+				return p.patchRecursive(typeOfT.Field(i).Name, targetValue.Field(i), tokens[1:], path)
 			}
 		}
 	case reflect.Array, reflect.Slice:
@@ -118,10 +189,20 @@ func (p *reflectPatcher) patchRecursive(fieldName string, targetValue reflect.Va
 	return nil, ""
 }
 
-func upperFirst(s string) string {
-	if s == "" {
-		return ""
+var emptyStruct = struct{}{}
+
+type segmentSet map[string]struct{}
+
+func (s *segmentSet) put(segs ...string) {
+	for _, seg := range segs {
+		(*s)[seg] = emptyStruct
 	}
-	r, n := utf8.DecodeRuneInString(s)
-	return string(unicode.ToUpper(r)) + s[n:]
+}
+
+func (s *segmentSet) asSlice() []string {
+	var segs []string
+	for seg := range *s {
+		segs = append(segs, seg)
+	}
+	return segs
 }
